38dd9751ce13b9c2fa044216f40b2121fa999514
ItBeeZ <bekezoltanrichard-20604@taszi.hu>
2025-11-27 10:13:48 +0100

    WPF fájlok inicializálva

---
+++ GithubWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GithubWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
